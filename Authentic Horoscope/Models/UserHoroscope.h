2b32a49eaba92d9739469e9ba0eb31cde886ca25
maxdavila <adavila0711@gmail.com>
2015-02-27 14:50:06 -0800

    Fixes folder structure

---
+++ Authentic Horoscope/Models/UserHoroscope.h
@@ -0,0 +1,18 @@
+//
+//  UserHoroscope.h
+//  Authentic Horoscope
+//
+//  Created by Max Davila on 2/22/15.
+//  Copyright (c) 2015 Max Davila. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserHoroscope : NSObject
+
++(UserHoroscope *)sharedInstance;
+@property(nonatomic, strong) NSString *snippetHoroscope;
+@property(nonatomic, strong) NSString *fullHoroscope;
+@property(nonatomic, strong) NSString *sign;
+@property(nonatomic, strong, readonly) NSString *setSignPrompt;
+@end
---
+++ Pods/Headers/Build/SKPolygraph/SKWord.h
@@ -0,0 +1,1 @@
+../../../SKPolygraph/SKPolygraph/SKWord.h
